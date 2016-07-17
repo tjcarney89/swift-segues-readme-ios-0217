64918022fffc3a5f8153988dbaabe597bb29bcaa
Michael Dippery <michael@monkey-robot.com>
2016-07-16 18:47:20 -0700

    Create sample project

---
+++ Fruits/Fruits/FruitListingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Fruits
+//
+//  Created by Michael Dippery on 7/16/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class FruitListingViewController: UITableViewController {
+}
